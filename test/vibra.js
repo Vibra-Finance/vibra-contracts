a67dea495525dff05caacbac492d0beee8766aeb
brianthf <blora616@gmail.com>
2021-08-07 22:29:23 -0400

    approval test

--- test/vibra.js
+++ test/vibra.js
@@ -9,25 +9,42 @@ const {
 const Vibra = artifacts.require("Vibra");
 
 contract("Vibra", async (accounts) => {
-    let instance;
-    const [admin, sender, reciever] = accounts;
-    this.value = new BN("5000000000000000000");
-    this.totalSupply = new BN("100000000000000000000000000");
+  let instance;
+  const [admin, smartContract, reciever] = accounts;
+  this.value = new BN("5000000000000000000");
+  this.totalSupply = new BN("100000000000000000000000000");
 
-    beforeEach(async () => {
-        instance = await Vibra.new({from: admin});
-    })
+  beforeEach(async () => {
+    instance = await Vibra.new({ from: admin });
+  });
 
-    it("Should create new token and mint 100,000,000 tokens", async () => {
-        let totalSupply = await instance.totalSupply.call();
-        
-        assert.equal(totalSupply.toString(), this.totalSupply.toString());
-    })
+  it("Should create new token and mint 100M tokens", async () => {
+    let totalSupply = await instance.totalSupply.call();
 
-    it("Should send 100,000,000 tokens to the admin address", async () => {
-        let balance = await instance.balanceOf.call(admin);
+    assert.equal(totalSupply.toString(), this.totalSupply.toString());
+  });
 
-        assert.equal(balance.toString(), this.totalSupply.toString());
-    })
+  it("Should send 100M tokens to the admin address", async () => {
+    let balance = await instance.balanceOf.call(admin);
 
-})
+    assert.equal(balance.toString(), this.totalSupply.toString());
+  });
+
+  it("Should send 5 tokens to the designated smart contract address", async () => {
+    await instance.transferFrom(admin, smartContract, this.value);
+
+    let balance = await instance.balanceOf(smartContract);
+
+    assert.equal(balance.toString(), this.value.toString());
+  });
+
+  it("Should approve the smart contract to spend 5 tokens", async () => {
+    let receipt = await instance.increaseAllowance(smartContract, this.value);
+
+    expectEvent(receipt, "Approval", {
+      owner: admin,
+      spender: smartContract,
+      value: this.value,
+    });
+  });
+});
