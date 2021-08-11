36304b5860a831fe48fcc6eb28cee9c261d8fc60
brianthf <blora616@gmail.com>
2021-08-10 22:56:34 -0400

    trust

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,12 +1,16 @@
 const Vibra = artifacts.require("Vibra");
 const Escrow = artifacts.require("Escrow");
+const Trust = artifacts.require("Trust");
 const { BN } = require("@openzeppelin/test-helpers");
 
 module.exports = async (deployer, _, accounts) => {
-  const [admin, buyer, seller] = accounts;
+  const [admin, buyer, seller, parent, child] = accounts;
   await deployer.deploy(Vibra, { from: admin });
   const vibra = await Vibra.deployed();
   await deployer.deploy(Escrow, vibra.address, new BN("1"), buyer, seller, {
+    from: admin,
+  });
+  await deployer.deploy(Trust, vibra.address, parent, child, new BN("1"), {
     from: admin,
   });
 };
